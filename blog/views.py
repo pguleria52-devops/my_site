c86a11f791a6122d32772220cbb5dd952ed01678
pguleria52-devops <partikshitg@gmail.com>
2025-06-22 20:07:50 +0530

    day2

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from datetime import date
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 all_posts = [
     {
@@ -75,21 +77,24 @@ def get_date(post):
 
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by('-date')[:3]
+    # sorted_posts = sorted(all_posts, key=get_date)
+    # latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {
       "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')[:3]
     return render(request, "blog/all-posts.html", {
       "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
       "post": identified_post
     })
